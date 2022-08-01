2d2df8bd493f9e45c117d73fb754df7c59f2d9cf
bbabilya <bjbabilya@gmail.com>
2022-08-01 15:40:26 -0500

    Initial commit

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,53 @@
+﻿//FOR Print 100
+for (int i = 1; i <= 255; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+//FOR Divisibles
+for (int i = 1; i <= 100; i++)
+{
+    if(i % 3 == 0 || i % 5 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+//FOR FizzBuzz
+for (int i = 1; i <= 100; i++)
+{
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if(i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+}
+
+
+//WHILE FizzBuzz
+int i = 0;
+while(i <= 100)
+{
+    i = i + 1;
+
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if(i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+}
